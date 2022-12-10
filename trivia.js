3e49366d8852d6eaf2bcc9bd410a7784cc3596fd
Luke Edmunds <Luke.edmunds49@ed.can.nl.ca>
2022-12-10 00:29:09 -0330

    final

--- trivia.js
+++ trivia.js
@@ -3,29 +3,65 @@
 const $ = selector => document.querySelector(selector);
 
 let btn = $(".text-right");
-let options = $("#option");
-let options2 = $("#flexRadioDefault1");
-let options3 = $("#flexRadioDefault2");
-let options4 = $("#flexRadioDefault3");
+
+
 //audio player
 const playAudio = (() => {
-    let audio = new Audio("https://cdn.uppbeat.io/audio-output/389/2116/main-version/streaming-previews/STREAMING-aura-tatami-main-version-02-18-7741.mp3");
-    audio.loop = true;
-    audio.volume = 0.2; 
-    audio.muted = false;
-    audio.play();
+  let audio = new Audio("https://cdn.uppbeat.io/audio-output/389/2116/main-version/streaming-previews/STREAMING-aura-tatami-main-version-02-18-7741.mp3");
+  audio.loop = true;
+  audio.volume = 0.2; 
+  audio.muted = false;
+  audio.play();
 }); 
-//image and text changer 
-const elementChanger = (()=>{
-    btn.addEventListener("click",() =>{
-        if (!$("input[name=flexRadioDefault]:checked" )) {
-            alert("Please select a radio button");
-          }else{
-            for (let index = 0; index < 10; index++){
-                console.log(randomIDs());
-          }}
-    });
-}); 
+
+
+
+const rightAnwers = (() => {
+  let correctAnswers = 0;
+  let totalQuestions = 10;
+  for (let i = 1; i <= totalQuestions; i++) {
+    let rightAnswer = $(`#RIGHT${i}`);
+    if (rightAnswer.checked) correctAnswers++;
+  }
+
+  // Calculate the number of incorrect answers
+  let incorrectAnswers = totalQuestions - correctAnswers;
+
+  // Create a new div element to display the results
+  const div = document.createElement('div');
+  div.classList.add('text-success');
+  div.style.color = "black";
+
+  // Create a Bootstrap h4 tag to display the results
+  const h4 = document.createElement('h4');
+  h4.innerText = `Correct answers: ${correctAnswers}/${totalQuestions}`;
+
+  // Add the h4 tag to the div element
+  div.append(h4);
+
+  // Add the div element to the page
+  btn.append(div);
+});
+
+
+const elementChecker = (() => {
+  btn.addEventListener("click", () => {
+    let isChecked = false;
+    for (let i = 1; i <= 10; i++) {
+      let rightAnswer = $(`#RIGHT${i}`);
+      if (rightAnswer.checked) {
+        isChecked = true;
+        break;
+      }
+    }
+    if (!isChecked) {
+      alert("Please select a radio button");
+    } else {
+      rightAnwers();
+    }
+  });
+});
+
 //custom alert function made with bootstrap
 const alert = (message, type) => {
   const wrapper = document.createElement('div')
@@ -38,14 +74,7 @@ const alert = (message, type) => {
 
   btn.append(wrapper)
 }
-const randomIDs = () => {
-    let idTags = [options, options2, options3, options4];
-    idTags.forEach(tag => {
-  let value = Math.random() < 0.5 ? "right" : "wrong";
-  $(tag).innerHTML = value;
-});
-}
 document.addEventListener("DOMContentLoaded",() =>{
 playAudio();
-elementChanger();
+elementChecker();
 });
